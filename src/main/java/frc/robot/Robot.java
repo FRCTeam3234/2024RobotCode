673af3409f0926bbd6b6b24770a2a92df9dfd22e
Superlamaface <39967561+Superlamaface@users.noreply.github.com>
2024-03-22 15:58:52 -0400

    Added more distance to angle before rotation

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -106,7 +106,7 @@ public class Robot extends TimedRobot {
         autonomousSequence.add(new DoNothingAutoAction());
       case autoSpeakerAngled:
         autonomousSequence.add(new ShootAutoAction(3.0, 1.0));
-        autonomousSequence.add(new MoveInlineAutoAction(2.0, 8.0, 2.0));
+        autonomousSequence.add(new MoveInlineAutoAction(2.0, 24.0, 2.0));
         autonomousSequence.add(new RotationAutoAction(0.0));
         autonomousSequence.add(new MoveInlineAutoAction(3.0, 80.0, 2.0));
         autonomousSequence.add(new DoNothingAutoAction());
